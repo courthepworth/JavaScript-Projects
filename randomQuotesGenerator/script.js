7d08f81f5ff5b4362ba88b06f850bddf98e95732
courthepworth <79781964+courthepworth@users.noreply.github.com>
2021-04-08 19:54:43 -0700

    Add files via upload

---
+++ randomQuotesGenerator/script.js
@@ -0,0 +1,48 @@
+// 4-8-21. 20 minutes to code. Spent an hour or more trying to figure out the CSS to lay it out the way I want. Need to learn CSS better, and will get back to it later.
+
+const quotes = [
+  {
+    quote:
+      "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+    author: " Life",
+  },
+  {
+    quote:
+      "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+    author: "John Kenneth Galbraith",
+  },
+  {
+    quote: "God save me from my friends. I can protect myself from my enemies.",
+    author: "Claude Louis Hector de Villars ",
+  },
+  {
+    quote: "The price of anything is the amount of life you exchange for it.",
+    author: "David Thoreau",
+  },
+  {
+    quote:
+      "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+    author: "Charles Lindbergh",
+  },
+  {
+    quote:
+      "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+    author: " Tyne Daly",
+  },
+];
+
+let x;
+
+document.querySelector(".quoteButton").addEventListener("click", function () {
+  assignQuote();
+});
+
+const randomNumber = function () {
+  return Math.trunc(Math.random() * quotes.length);
+};
+
+const assignQuote = function () {
+  x = randomNumber();
+  document.querySelector(".quoteText").textContent = quotes[x].quote;
+  document.querySelector(".author").textContent = quotes[x].author;
+};
